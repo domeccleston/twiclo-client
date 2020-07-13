777263890bc6f2b1d420d3f9b6fd28631b87f9ae
Dom Eccleston <domeccleston@gmail.com>
2020-07-13 18:32:00 +0100

    adding form validation

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,20 +1,40 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import '../index.css';
 import {
   Heading,
   Flex,
   Image,
   FormControl,
+  FormLabel,
+  FormErrorMessage,
   Input,
   Button,
 } from '@chakra-ui/core';
 import coolGuy from '../images/cool-guy.jpg';
 
 const Form = () => {
+  const { handleSubmit, errors, register, formState } = useForm();
+
+  function onSubmit(values) {
+    setTimeout(() => {
+      alert(JSON.stringify(values, null, 2));
+    }, 1000);
+  }
+
+  function validateEmail(value) {
+    let error;
+    if (!value) {
+      error = 'Please enter email!';
+    }
+    return error || true;
+  }
+
   return (
     <Flex w="100%" h="100vh">
       <Flex w="45%" h="100vh" direction="column">
         <Flex w="100%" h="10vh" align="center">
-          <Heading p={5} color="#3B3187">
+          <Heading p={5} color="#3B3187" fontFamily="Rubik">
             Twiclo
           </Heading>
         </Flex>
@@ -46,15 +66,40 @@ const Form = () => {
                   Sign up
                 </Heading>
               </Flex>
-              <FormControl>
-                <Input m={2} bg="#F2F6FD" />
-                <Input m={2} bg="#F2F6FD" />
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <FormControl isRequired>
+                  <Input
+                    name="email"
+                    placeholder="Email address"
+                    ref={register({ validate: validateEmail })}
+                    m={2}
+                    bg="#F2F6FD"
+                  />
+                </FormControl>
+                <FormControl>
+                  <Input
+                    name="password"
+                    placeholder="Password"
+                    ref={register}
+                    type="password"
+                    m={2}
+                    bg="#F2F6FD"
+                  />
+                </FormControl>
                 <Flex w="100%" justify="center">
-                  <Button size="lg" m={2} width="50%" bg="#FB5463" color="#fff">
+                  <Button
+                    type="submit"
+                    isLoading={formState.isSubmitting}
+                    size="lg"
+                    m={2}
+                    width="50%"
+                    bg="#FB5463"
+                    color="#fff"
+                  >
                     Log in
                   </Button>
                 </Flex>
-              </FormControl>
+              </form>
             </Flex>
           </Flex>
         </Flex>
